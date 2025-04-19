258db79747297c681c80ee0dba608bd0f27f18c7
conanjir <conanjrichards@gmail.com>
2025-04-19 14:21:51 +1000

    Update cat.js

--- terminal-content/cat/cat.js
+++ terminal-content/cat/cat.js
@@ -1,50 +1,31 @@
+let bottomImg, topImg;
+
+function preload()  {
+  bottomImg = loadImage('./data/catbw.jpeg');
+  topImg = loadImage('./data/cat.jpg');
+
+}
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background("white");
+  background(200, 200, 0);
+  image(bottomImg, 0, 0);
   noStroke();
   frameRate(1);
 }
 
+
 function draw() {
-  background("white");
 
-  let maxTries = 100;
-  let circleCount = 10;
-  let placed = 0;
-  let spacing = map(mouseX, 0, width, 20, 100);
+}
 
-  randomSeed(mouseY);
+function mouseDragged() {
 
-  while (placed < circleCount && maxTries > 0) {
-    let x = random(width);
-    let y = random(height);
-    let ok = true;
 
-    for (let i = 0; i < placed; i++) {
-      let otherX = circleX[i];
-      let otherY = circleY[i];
-      let d = dist(x, y, otherX, otherY);
-      if (d < spacing) {
-        ok = false;
-        break;
-      }
-    }
+  copy(topImg, mouseX, mouseY, 80, 80, mouseX, mouseY, 160, 160);
 
-    if (ok) {
-      fill(255, 0, 0);
-      circle(x, y, spacing);
-      circleX[placed] = x;
-      circleY[placed] = y;
-      placed++;
-    }
-
-    maxTries--;
-  }
 }
 
 function windowResized() {
-  resizeCanvas(windowWidth, windowHeight);
+  resizeCanvas(window.innerWidth, window.innerHeight);
 }
-
-let circleX = [];
-let circleY = [];
